3d4f3b054b78b5736fdc3df186be567b01180896
mehroj-saparov-io <saparov.dev2026@gmail.com>
2026-02-17 21:28:43 +0500

    category and post models, admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Post, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+    search_fields = ("name",)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "is_published",
+        "reading_time",
+        "views",
+        "created_at",
+        "updated_at",
+        "image_tag",
+    )
+    list_filter = (
+        "is_published",
+        "categories",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("title", "excerpt", "content")
+    readonly_fields = ("views", "reading_time", "created_at", "updated_at", "image_tag")
+
+    filter_horizontal = ("categories",)
+    prepopulated_fields = {"slug": ("title",)}
+    ordering = ("-created_at",)
+
+    def image_tag(self, obj):
+        if obj.base_image:
+            return format_html('<img src="{}" width="80" style="object-fit: cover;"/>', obj.base_image.url)
+        return "-"
+    image_tag.short_description = "Rasm"
+
+    actions = ["make_published", "make_unpublished"]
+
+    def make_published(self, request, queryset):
+        updated = queryset.update(is_published=True)
+        self.message_user(request, f"{updated} ta post published qilindi")
+    make_published.short_description = "Belgilangan postlarni publish qilish"
+
+    def make_unpublished(self, request, queryset):
+        updated = queryset.update(is_published=False)
+        self.message_user(request, f"{updated} ta post unpublish qilindi")
+    make_unpublished.short_description = "Belgilangan postlarni unpublish qilish"
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.utils.text import slugify
+import math
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+
+    categories = models.ManyToManyField(
+        Category,
+        related_name="posts",
+        blank=True
+    )
+
+    base_image = models.ImageField(
+        upload_to="blog/",
+        blank=True,
+        null=True,
+        help_text="Post uchun asosiy rasm"
+    )
+
+    excerpt = models.TextField(max_length=300, blank=True)
+    content = models.TextField()
+
+    reading_time = models.PositiveIntegerField(default=1)
+    views = models.PositiveIntegerField(default=0)
+
+    is_published = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["-created_at"]),
+            models.Index(fields=["views"]),
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            i = 1
+            while Post.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{i}"
+                i += 1
+            self.slug = slug
+
+        if self.content:
+            words = len(self.content.split())
+            self.reading_time = max(1, math.ceil(words / 180))
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
